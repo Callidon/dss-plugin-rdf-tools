e7a95dbb440e4c180c36f947ea839bfa32600278
thomas.minier@dataiku.com <thomas.minier@dataiku.com>
2025-10-04 10:37:32 +0000

    Edited file 'python-lib/dkurdftools/rdf_format_extractor.py'

---
+++ python-lib/dkurdftools/rdf_format_extractor.py
@@ -0,0 +1,44 @@
+from dataiku.customformat import FormatExtractor
+from rdflib import Graph
+        
+
+class RDFFormatExtractor(FormatExtractor):
+    """
+    Reads a stream in a format to a stream of rows
+    """
+    def __init__(self, rdf_format, stream, schema):
+        """
+        Initialize the extractor
+        :param stream: the stream to read the formatted data from
+        """
+        FormatExtractor.__init__(self, stream)
+        self.graph = Graph()
+        self.columns = ["subject", "predicate", "object"]
+        
+        # parse file content
+        file_content = "\n".join([line.decode('utf-8') for line in stream.readlines()])
+        self.graph.parse(data=file_content, format=rdf_format)
+        
+        # create an iterator over the graph content
+        self.iterator = iter(self.graph)
+        
+    def read_schema(self):
+        """
+        Get the schema of the data in the stream, if the schema can be known upfront.
+        """
+        return [{"name": "subject", "type": "STRING"},
+                {"name": "predicate", "type": "STRING"},
+               {"name": "object", "type": "STRING"}]
+    
+    def read_row(self):
+        """
+        Read one row from the formatted stream
+        :returns: a dict of the data (name, value), or None if reading is finished
+        """
+        try:
+            s, p, o = next(self.iterator)
+            return {"subject": s, "predicate": p, "object": o}
+        except StopIteration:
+            pass
+        return None
+        
