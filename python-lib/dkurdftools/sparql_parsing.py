c80b3e7494d17526388b711b9a61ad631b5e1a63
thomas.minier@dataiku.com <thomas.minier@dataiku.com>
2025-10-05 17:09:36 +0000

    Edited file 'python-lib/dkurdftools/sparql_parsing.py'

--- python-lib/dkurdftools/sparql_parsing.py
+++ python-lib/dkurdftools/sparql_parsing.py
@@ -6,19 +6,37 @@ from rdflib.plugins.sparql.algebra import translateAlgebra
 from rdflib.plugins.sparql.sparql import Query
 
 def parse_query(query: str) -> Query:
-    """Parse a string SPARQL query into a logical query plan"""
+    """Parse a string SPARQL query into a logical query plan
+    
+    :param query: String SPARQL query
+    :return SPARQL logical query plan
+    """
     return translateQuery(parseQuery(query))
 
 
 def unparse_query(parsed_query: Query) -> str:
-    """Turn a logical SPARQL query plan into a string SPARQL query"""
+    """Turn a logical SPARQL query plan into a string SPARQL query
+    
+    :param parsed_query: SPARQL logical query plan
+    :return String SPARQL query
+    """
     return translateAlgebra(parsed_query)
 
 
 def is_query_select_type(parsed_query: Query) -> bool:
+    """Test if a SPARQL query plan is a Select query
+    
+    :param parsed_query: SPARQL logical query plan
+    :return True if the query is a Select query, False otherwise
+    """
     return parsed_query.algebra.name == "Select"
 
 def is_query_construct_type(parsed_query: Query) -> bool:
+    """Test if a SPARQL query plan is a Construct query
+    
+    :param parsed_query: SPARQL logical query plan
+    :return True if the query is a Construct query, False otherwise
+    """
     return parsed_query.algebra.name == "Construct"
 
 
