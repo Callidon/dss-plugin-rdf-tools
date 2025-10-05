331922e27ee905b74c5fcabcc6fa099846317ea9
thomas.minier@dataiku.com <thomas.minier@dataiku.com>
2025-10-05 06:51:16 +0000

    Edited file 'python-lib/dkurdftools/sparql_parsing.py'

--- python-lib/dkurdftools/sparql_parsing.py
+++ python-lib/dkurdftools/sparql_parsing.py
@@ -11,8 +11,11 @@ def unparse_query(parsed_query: str) -> dict:
     return {} # TODO
 
 
-def check_query_is_select_or_construct(parsed_query: str):
-    return parsed_query.name == "Select" or parsed_query.name == "Construct"
+def is_query_select_type(parsed_query: str):
+    return parsed_query.name == "Select"
+
+def is_query_construct_type(parsed_query: str):
+    return parsed_query.name == "Construct"
 
 
 def add_limit_to_query(parsed_query: dict, limit: int) -> dict:
