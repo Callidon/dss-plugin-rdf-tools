6d7772e217f7aa225f181885955bc3b9c33bb7cf
Thomas Minier <thomas.minier@dataiku.com>
2025-10-08 13:47:51 +0200

    put directory in the right spot

--- python-connectors/dku-rdf-tools_dku-sparql-dataset/resource/sparql_input.js
+++ python-connectors/dku-rdf-tools_dku-sparql-dataset/resource/sparql_input.js
@@ -3,6 +3,6 @@ var app = angular.module("dkurdftools.sparqlinput", []);
 app.controller("SparqlInputFormController", function ($scope) {
   const yasgui = new Yasgui(document.getElementById("sparql_query_yasgui"));
   yasgui.on("change", (args) => {
-    $scope.config.spaql_query = args.getQueryWithValues();
+    $scope.config.sparql_query = args.getQueryWithValues();
   });
 });
