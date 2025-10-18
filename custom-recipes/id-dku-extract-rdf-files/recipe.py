989da9d3fcb20e714c89aac64561098f3e9f22e3
Thomas Minier <thomas.minier@dataiku.com>
2025-10-18 10:56:03 +0200

    chore: wip on extract rdf files recipe

--- custom-recipes/id-dku-extract-rdf-files/recipe.py
+++ custom-recipes/id-dku-extract-rdf-files/recipe.py
@@ -1,82 +1,63 @@
-# Code for custom code recipe id-dku-rdf-files-extractor (imported from a Python recipe)
-
-# To finish creating your custom recipe from your original PySpark recipe, you need to:
-#  - Declare the input and output roles in recipe.json
-#  - Replace the dataset names by roles access in your code
-#  - Declare, if any, the params of your custom recipe in recipe.json
-#  - Replace the hardcoded params values by acccess to the configuration map
-
-# See sample code below for how to do that.
-# The code of your original recipe is included afterwards for convenience.
-# Please also see the "recipe.json" file for more information.
-
-# import the classes for accessing DSS objects from the recipe
+# Code for custom code recipe id-dku-rdf-files-extractor
+from typing import Literal
 import dataiku
+import pandas as pd
+from rdflib import Graph
+
+from dkurdftools.formats.utils import parse_rdf_stream_as_graph
+
 # Import the helpers for custom recipes
 from dataiku.customrecipe import get_input_names_for_role
 from dataiku.customrecipe import get_output_names_for_role
 from dataiku.customrecipe import get_recipe_config
 
+
 # Inputs and outputs are defined by roles. In the recipe's I/O tab, the user can associate one
-# or more dataset to each input and output role.
-# Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
-
-# To  retrieve the datasets of an input role named 'input_A' as an array of dataset names:
-input_managed_folders_names = get_input_names_for_role('input_managed_folders')
+input_managed_folders_names = get_input_names_for_role("input_managed_folders")
 input_managed_folders = [dataiku.Folder(name) for name in input_managed_folders_names]
-
 # There's only one unary output in this recipe
-output_dataset_names = get_output_names_for_role('output_dataset')
+output_dataset_names = get_output_names_for_role("output_dataset")
 output_dataset = dataiku.Dataset(output_dataset_names[0])
 
-
-# The configuration consists of the parameters set up by the user in the recipe Settings tab.
-
-# Parameters must be added to the recipe.json file so that DSS can prompt the user for values in
-# the Settings tab of the recipe. The field "params" holds a list of all the params for wich the
-# user will be prompted for values.
-
-# The configuration is simply a map of parameters, and retrieving the value of one of them is simply:
-my_variable = get_recipe_config()['parameter_name']
-
-# For optional parameters, you should provide a default value in case the parameter is not present:
-my_variable = get_recipe_config().get('parameter_name', None)
-
-# Note about typing:
-# The configuration of the recipe is passed through a JSON object
-# As such, INT parameters of the recipe are received in the get_recipe_config() dict as a Python float.
-# If you absolutely require a Python int, use int(get_recipe_config()["my_int_param"])
-
-
-#############################
-# Your original recipe
-#############################
-
-# -*- coding: utf-8 -*-
-import dataiku
-from os.path import join
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
-from rdflib import Graph
-
-
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+# Read parameters (see recipe.json for details). the first two are mandatory
+output_schema: Literal["sql_table", "single_column"] = get_recipe_config().get(
+    "output_schema"
+)
+single_column_output = get_recipe_config().get("single_column_output")
+single_column_output_format = get_recipe_config().get(
+    "single_column_output_format", "turtle"
+)
+# file_source_output_column is optional and can be None
+file_source_output_column = get_recipe_config().get("file_source_output_column")
 
 # Compute a Pandas dataframe to write into the output dataset
-output_df = pd.DataFrame(columns=["subject", "predicate", "object"])
+if output_schema == "sql_table":
+    columns = ["subject", "predicate", "object"]
+else:
+    columns = [single_column_output]
+if file_source_output_column is not None:
+    columns.append(file_source_output_column)
+output_df = pd.DataFrame(columns=columns)
 
 
 for input_managed_folder in input_managed_folders:
     for file_path in input_managed_folder.list_paths_in_partition():
-        g = Graph()
-        with RDF_sources.get_download_stream(file_path) as f:
-            g.parse(f.read())
+        # load grpah form the file
+        with input_managed_folder.get_download_stream(file_path) as stream:
+            graph: Graph = parse_rdf_stream_as_graph(stream, file_format=None)
 
-for s, p, o in g:
-    output_df.loc[len(output_df)]= [s, p, o]
-
+        # extract the graph into the output dataset
+        if output_schema == "sql_table":
+            for s, p, o in graph:
+                data = [s.n3(), p.n3(), o.n3()]
+                if file_source_output_column is not None:
+                    data.append(file_path)
+                output_df.loc[len(output_df)] = data
+        else:  # else, serialize the whole graph into a single column
+            data = [graph.serialize(format=single_column_output_format)]
+            if file_source_output_column is not None:
+                data.append(file_path)
+            output_df.loc[len(output_df)] = data
 
 # Write recipe outputs
 output_dataset.write_with_schema(output_df)
--- python-lib/dkurdftools/formats/utils.py
+++ python-lib/dkurdftools/formats/utils.py
@@ -1,15 +1,15 @@
-from typing import IO, Literal
+from typing import IO, Literal, Optional
 
 from rdflib import Graph
 
 
 def parse_rdf_stream_as_graph(
-    stream: IO, file_format: Literal["xml", "n3", "nt", "trix"]
+    stream: IO, file_format: Optional[Literal["xml", "n3", "nt", "trix"]]
 ) -> Graph:
     """Parse a stream of RDF data as an rdflib Graph
 
     :param stream: Stream of RDF data
-    :param file_format: File format
+    :param file_format: File format. If set to None, rdflib will try to guess the format
     :return: Graph loaded with the file content
     """
     graph = Graph()    
