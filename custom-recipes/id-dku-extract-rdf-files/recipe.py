7d8e9250a2a993125c6d78a6be13ab25143a8adc
Thomas Minier <thomas.minier@dataiku.com>
2025-10-19 10:38:43 +0200

    chore(recipe/extract-rdf-file): write a dss native store to have a unified way of writing RDF graph to a DSS dataset

--- custom-recipes/id-dku-extract-rdf-files/recipe.py
+++ custom-recipes/id-dku-extract-rdf-files/recipe.py
@@ -1,10 +1,8 @@
 # Code for custom code recipe id-dku-rdf-files-extractor
-from typing import Literal
 import dataiku
-import pandas as pd
 from rdflib import Graph
 
-from dkurdftools.formats.utils import parse_rdf_stream_as_graph
+from dkurdftools.storage.dss_store import DataikuDatasetStore
 
 # Import the helpers for custom recipes
 from dataiku.customrecipe import get_input_names_for_role
@@ -19,45 +17,30 @@ input_managed_folders = [dataiku.Folder(name) for name in input_managed_folders_
 output_dataset_names = get_output_names_for_role("output_dataset")
 output_dataset = dataiku.Dataset(output_dataset_names[0])
 
-# Read parameters (see recipe.json for details). the first two are mandatory
-output_schema: Literal["sql_table", "single_column"] = get_recipe_config().get(
-    "output_schema"
+# Read parameters (see recipe.json for details)
+subject_output_column = get_recipe_config().get("subject_output_column", "subject")
+predicate_output_column = get_recipe_config().get(
+    "predicate_output_column", "predicate"
 )
-single_column_output = get_recipe_config().get("single_column_output")
-single_column_output_format = get_recipe_config().get(
-    "single_column_output_format", "turtle"
+object_output_column = get_recipe_config().get("object_output_column", "object")
+
+# use the dedicated dataiku dataset store for the graph
+# which will take care of writing the output into the dataset
+store = DataikuDatasetStore(
+    output_dataset,
+    subject_column_name=subject_output_column,
+    predicate_column_name=predicate_output_column,
+    object_column_name=object_output_column,
 )
-# file_source_output_column is optional and can be None
-file_source_output_column = get_recipe_config().get("file_source_output_column")
+# init the dataset schema
+store.write_schema()
+graph = Graph(store=store)
 
-# Compute a Pandas dataframe to write into the output dataset
-if output_schema == "sql_table":
-    columns = ["subject", "predicate", "object"]
-else:
-    columns = [single_column_output]
-if file_source_output_column is not None:
-    columns.append(file_source_output_column)
-output_df = pd.DataFrame(columns=columns)
-
-
+# load each file into the graph
 for input_managed_folder in input_managed_folders:
     for file_path in input_managed_folder.list_paths_in_partition():
-        # load grpah form the file
         with input_managed_folder.get_download_stream(file_path) as stream:
-            graph: Graph = parse_rdf_stream_as_graph(stream, file_format=None)
+            graph.parse(data=stream.read())
 
-        # extract the graph into the output dataset
-        if output_schema == "sql_table":
-            for s, p, o in graph:
-                data = [s.n3(), p.n3(), o.n3()]
-                if file_source_output_column is not None:
-                    data.append(file_path)
-                output_df.loc[len(output_df)] = data
-        else:  # else, serialize the whole graph into a single column
-            data = [graph.serialize(format=single_column_output_format)]
-            if file_source_output_column is not None:
-                data.append(file_path)
-            output_df.loc[len(output_df)] = data
-
-# Write recipe outputs
-output_dataset.write_with_schema(output_df)
+# commit any remaining data
+graph.commit()
